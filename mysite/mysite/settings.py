57591a36a5f9119055ffa51860671f3e6bb9c0aa
daggeru <dagett90@o2.pl>
2017-03-19 17:27:52 +0100

    poprawka do produkcji

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2iyyrw(u6cl$!+=+wpqy)y#&34)(hu@*ua@)s(u&tcshx9qpvw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
