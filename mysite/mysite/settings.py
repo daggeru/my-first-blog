11a300d7c59f44ea9bc4ff4bf1b33534bef005a6
daggeru <dagett90@o2.pl>
2017-03-19 18:16:57 +0100

    kolejny commit z cssem

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2iyyrw(u6cl$!+=+wpqy)y#&34)(hu@*ua@)s(u&tcshx9qpvw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -102,3 +102,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
